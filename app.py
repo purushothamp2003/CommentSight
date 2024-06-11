8943ec72c41f8de60de9590a8c6c278e30fa8595
DsouzaSeema <142740216+DsouzaSeema@users.noreply.github.com>
2024-06-11 22:33:00 +0530

    modified app.py and webscrapping

--- app.py
+++ app.py
@@ -1,9 +1,81 @@
-from flask import Flask , render_template
 
+from flask import Flask, request, render_template, jsonify
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import logging
 
 app = Flask(__name__)
 
+# Setup logging
+logging.basicConfig(level=logging.DEBUG)
+
+api_key = 'AIzaSyBGmU4wRfLptG1mN0B2wlVv1QCMUADRRL8'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Function to get comments from a YouTube video
+def get_comments(video_id, max_results=100):
+    comments = []
+    next_page_token = None
+
+    try:
+        while True:
+            logging.debug("Fetching comments page")
+            # Fetch comments from the YouTube API
+            request = youtube.commentThreads().list(
+                part='snippet',
+                videoId=video_id,
+                pageToken=next_page_token,
+                maxResults=max_results,
+                textFormat='plainText'
+            )
+            response = request.execute()
+            logging.debug("Response received: %s", response)
+
+            # Parse the response and extract comments
+            for item in response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                comments.append(comment)
+                logging.debug("Comment: %s", comment)
+
+            # Check if there are more pages of comments
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+    except HttpError as e:
+        logging.error("An HTTP error occurred: %s", e)
+    except Exception as e:
+        logging.error("An error occurred: %s", e)
+
+    return comments
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['GET'])
+def results():
+    url = request.args.get('url')
+    # Extract the video ID from the URL
+    video_id = extract_video_id(url)
+    if video_id:
+        comments = get_comments(video_id)
+        return jsonify(comments)
+    else:
+        return jsonify({'error': 'Invalid URL'}), 400
+
+
+from urllib.parse import urlparse, parse_qs
+
+def extract_video_id(url):
+    parsed_url = urlparse(url)
+    if parsed_url.netloc == 'youtu.be':
+        return parsed_url.path.lstrip('/')
+    else:
+        query = parsed_url.query
+        params = parse_qs(query)
+        return params.get('v', [None])[0]
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- webscrapping.py
+++ webscrapping.py
@@ -1,4 +1,10 @@
+
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.DEBUG)
 
 api_key = 'AIzaSyBGmU4wRfLptG1mN0B2wlVv1QCMUADRRL8'
 video_id = '4mXgiOc4PU0'
@@ -10,30 +16,40 @@ def get_comments(video_id, max_results=100):
     comments = []
     next_page_token = None
 
-    while True:
-        # Fetch comments from the YouTube API
-        request = youtube.commentThreads().list(
-            part='snippet',
-            videoId=video_id,
-            pageToken=next_page_token,
-            maxResults=max_results,
-            textFormat='plainText'
-        )
-        response = request.execute()
+    try:
+        while True:
+            logging.debug("Fetching comments page")
+            # Fetch comments from the YouTube API
+            request = youtube.commentThreads().list(
+                part='snippet',
+                videoId=video_id,
+                pageToken=next_page_token,
+                maxResults=max_results,
+                textFormat='plainText'
+            )
+            response = request.execute()
+            logging.debug("Response received: %s", response)
 
-        # Parse the response and extract comments
-        for item in response['items']:
-            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-            comments.append(comment)
+            # Parse the response and extract comments
+            for item in response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                comments.append(comment)
+                logging.debug("Comment: %s", comment)
 
-        # Check if there are more pages of comments
-        next_page_token = response.get('nextPageToken')
-        if not next_page_token:
-            break
+            # Check if there are more pages of comments
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+    except HttpError as e:
+        logging.error("An HTTP error occurred: %s", e)
+    except Exception as e:
+        logging.error("An error occurred: %s", e)
 
     return comments
 
 # Fetch the comments for the specified video
+logging.info("Fetching comments for video ID: %s", video_id)
 comments = get_comments(video_id)
 
 # Print the list of comments
